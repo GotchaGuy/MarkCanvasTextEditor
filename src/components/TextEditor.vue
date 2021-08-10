7a3e82b9f220a281189aed5954ce59e3bf1513eb
GotchaGuy <aljosa100@gmail.com>
2021-08-10 14:41:14 +0200

    vue & tailwind init

---
+++ src/components/TextEditor.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TextEditor',
+  props: {},
+  data() {
+    return {
+      textArray: [
+        {
+          content: "",
+          x: "",
+          y: "",
+          font: "",
+          size: "",
+          lineHeight: "",
+        },
+      ],
+    }
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
