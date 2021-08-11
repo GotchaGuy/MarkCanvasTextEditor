efba999d5ac6087c705404c7b0ebb3f916bd1b6a
GotchaGuy <aljosa100@gmail.com>
2021-08-11 17:41:30 +0200

    adding and manipulating objects

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--  <img alt="Vue logo" src="./assets/logo.png">-->
+<!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <text-editor/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import TextEditor from "./components/TextEditor";
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TextEditor
   }
 }
 </script>
+<!--<script src="./lib/fabric.min.js"></script>-->
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
+  /*-webkit-font-smoothing: antialiased;*/
+  /*-moz-osx-font-smoothing: grayscale;*/
+  /*text-align: center;*/
+  /*color: #2c3e50;*/
+  /*margin-top: 60px;*/
 }
 </style>
--- src/components/TextEditor.vue
+++ src/components/TextEditor.vue
@@ -1,10 +1,41 @@
 <template>
-  <div class="container">
-
+  <div class="container w-4/5 mx-auto">
+    <div class="header w-full h-24 bg-green-400">
+      <h1>Text Editor</h1>
+    </div>
+    <section id="main flex flex-row flex-nowrap min-h-screen">
+      <div class="w-4/5 bg-gray-300 m-0 h-full">
+        <!--        <div class="canvas-container h-80 w-80 bg-white">-->
+        <canvas id="canvas" class=" mx-auto my-auto"></canvas>
+        <!--        </div>-->
+      </div>
+      <div class="w-1/5 h-full">
+        <h3>Text Layer</h3>
+        <label for="content">Enter text</label>
+        <input type="text" id="content" name="content" v-model="textArray[0].content" @change="changeObject()">
+        <div>
+          <label for="size">Font size</label>
+          <input type="text" id="size" name="size" v-model="textArray[0].size" @change="textbox.set({size: textArray[0].size})">
+        </div>
+        <div>
+          <label for="content">Line Height</label>
+          <input type="text" id="lineHeight" name="lineHeight" v-model="textArray[0].lineHeight">
+        </div>
+        <div>
+          <label for="content">Change Font Family</label>
+          <input type="text" id="fontFam" name="fontFam" v-model="textArray[0].fontFam">
+        </div>
+      </div>
+    </section>
+    <div class="bg-blue-500">
+      <p>{{ textArray[0].content }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import {fabric} from 'fabric'
+
 export default {
   name: 'TextEditor',
   props: {},
@@ -12,17 +43,59 @@ export default {
     return {
       textArray: [
         {
-          content: "",
+          content: "words words",
           x: "",
           y: "",
-          font: "",
+          fontFam: "",
           size: "",
           lineHeight: "",
         },
       ],
+      textbox: {}
     }
 
-  }
+  },
+  mounted() {
+    // const canvas = new fabric.Canvas('canvas', {
+    //   width: 500,
+    //   height: 500,
+    //   backgroundColor: 'white'
+    // });
+    const canvas = this.initCanvas('canvas');
+    canvas.renderAll();
+
+    // canvas.on('mouse:over', (e) => {
+    //   console.log(e);
+    // })
+// 'This is a Textbox object'
+    this.textbox = new fabric.Textbox( this.textArray[0].content , {
+      left: 50,
+      top: 50,
+      fill: '#880E4F',
+      stroke: "#D81B60",
+      strokeWidth: 2,
+      size: 16,
+      fontFamily: "Roboto",
+      lineHeight: 0.5,
+    });
+
+    canvas.add(this.textbox);
+
+    console.log(this.textbox);
+
+  },
+  methods: {
+    initCanvas(id) {
+      return new fabric.Canvas(id, {
+        width: 500,
+        height: 500,
+        backgroundColor: 'white'
+      });
+    },
+    changeObject() {
+      // canvas.renderAll();
+    }
+  },
 }
 </script>
 
